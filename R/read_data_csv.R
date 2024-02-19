582a98ca87392fb27c450223525939dd1d0b324b
Deepak Saini <saini1397@gmail.com>
2024-02-19 03:44:41 +0000

    Commented Source

--- R/read_data_csv.R
+++ R/read_data_csv.R
@@ -1,6 +1,6 @@
-library(readr)
+#library(readr)
 
-saveRDS(read_csv("C:/Users/Ayansh/Downloads/biodiversity-data/occurence.csv",n_max=500),file="occurence.rds", compress = TRUE )
+#saveRDS(read_csv("C:/Users/Ayansh/Downloads/biodiversity-data/occurence.csv",n_max=500),file="occurence.rds", compress = TRUE )
 
 
-saveRDS(read_csv("C:/Users/Ayansh/Downloads/biodiversity-data/multimedia.csv",n_max=500),file="multimedia.rds", compress = TRUE )
+#saveRDS(read_csv("C:/Users/Ayansh/Downloads/biodiversity-data/multimedia.csv",n_max=500),file="multimedia.rds", compress = TRUE )

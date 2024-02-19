f5dfc66e54b7033d64b5f84eaa1cf1363b3f93c5
Deepak Saini <saini1397@gmail.com>
2024-02-19 13:22:44 +0530

    Added Readme

--- R/read_data_csv.R
+++ R/read_data_csv.R
@@ -1,9 +1,9 @@
-# Use this to Load the data from CSV
+#Use this to Load the data from CSV
 
 
-#library(readr)
+library(readr)
 
-#saveRDS(read_csv("C:/Users/Ayansh/Downloads/biodiversity-data/occurence.csv",n_max=500),file="occurence.rds", compress = TRUE )
+saveRDS(read_csv("C:/Users/Ayansh/Downloads/biodiversity-data/occurence.csv",n_max=500000),file="occurence.rds", compress = TRUE )
 
 
 #saveRDS(read_csv("C:/Users/Ayansh/Downloads/biodiversity-data/multimedia.csv",n_max=500),file="multimedia.rds", compress = TRUE )
